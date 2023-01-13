9504cafa6e962313f6aaba282b0f93e7727650a7
FracPDE_UofSC <122580422+FracPDE-UofSC@users.noreply.github.com>
2023-01-12 21:37:28 -0500

    Add files via upload

---
+++ com_AL.m
@@ -0,0 +1,20 @@
+function y = com_AL(g,n)
+%% Compute  left  fractional differential 
+AL=zeros(n-1,n-1);
+for i=1:n-1
+    for j=1:n-1
+        if i==j
+            AL(i,j)=g(2);
+        elseif j==i-1
+            AL(i,j)=g(3);
+        elseif i==j-1
+            AL(i,j)=g(1);
+        elseif j<i-1
+            AL(i,j)=g(i-j+2);
+        else
+            AL(i,j)=0;
+        end
+    end
+end
+y = AL;
+end
